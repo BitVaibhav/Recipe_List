7e54ddb99b85dccbd318e2ff3cc496e485dd4310
Vaibhav <vbhchaurasia001@gmail.com>
2021-12-24 16:16:03 +0530

    initial commit

---
+++ lib/models/recipe_api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "cd6ec46abamsh0edd3f1161e6752p1ebed9jsn257c0f8d9995",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
